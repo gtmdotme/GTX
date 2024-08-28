826e22980b85cce26e2eba2bc0c2aa405fa1c398
Gautam Choudhary <gc.iitr@gmail.com>
2024-08-28 03:07:27 -0400

    updated test_all example to include pyg

--- examples/test_all/main.cpp
+++ examples/test_all/main.cpp
@@ -1,8 +1,11 @@
+#include <torch/script.h>
+#include <torchscatter/scatter.h>
+#include <torchsparse/sparse.h>
 #include <iostream>
 #include "GTX.hpp"
 
 // using GTX = gt::Graph;
-int main() {
+int main(int argc, const char* argv[]) {
     // GTX g;
     gt::Graph g;
 
@@ -150,6 +153,32 @@ int main() {
     // auto handler = g.get_sssp_handler(max_vertex_id);
     // double delta = 2.0;
     // handler.compute(root,delta);
+
+    if (argc != 2) {
+        std::cerr << "usage: hello-world <path-to-exported-script-module>\n";
+        return -1;
+    }
+
+    torch::jit::script::Module model;
+    try {
+        model = torch::jit::load(argv[1]);
+    } catch (const c10::Error &e) {
+        std::cerr << "error loading the model\n";
+        return -1;
+    }
+
+    auto x = torch::randn({5, 32});
+    auto edge_index = torch::tensor({
+        {0, 1, 1, 2, 2, 3, 3, 4},
+        {1, 0, 2, 1, 3, 2, 4, 3},
+    });
+
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(x);
+    inputs.push_back(edge_index);
+
+    auto out = model.forward(inputs).toTensor();
+    std::cout << "output tensor shape: " << out.sizes() << std::endl;
     
     return 0;
 }
